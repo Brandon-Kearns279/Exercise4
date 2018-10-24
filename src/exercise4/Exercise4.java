3657f177a56dc00397e638cfb3f6b36a97b86e86
Brandon-Kearns279 <32343611+Brandon-Kearns279@users.noreply.github.com>
2018-10-24 15:46:49 -0400

    Commit One

---
+++ src/exercise4/Exercise4.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise4;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Brandon Kearns
+ */
+public class Exercise4 {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "    /\\**/\\\n" +
+"   ( o_o  )_)\n" +
+"   ,(u  u  ,),\n" +
+"  {}{}{}{}{}{}");
+    }
+    
+}
